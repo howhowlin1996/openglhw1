0f907459cc5c384c087f860c0fa429a71cff536a
KenSi <keff456789@gmail.com>
2020-03-17 17:56:25 +0800

    Initial Commit

---
+++ OpenGLFramework-Xcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLFramework-Xcode
+//
+//  Created by 施少強 on 2020/3/17.
+//  Copyright © 2020 CGTAs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
